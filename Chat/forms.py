72d5d8b69eb62954ad360d666b5e7637ae72ae2b
AlazarBekele <alazarthe70@gmail.com>
2024-12-23 00:17:27 +0300

    add submit button, collectstatic, creat signup login pages: Try to center align

--- Chat/forms.py
+++ Chat/forms.py
@@ -6,32 +6,32 @@ from django.contrib.auth.models import User
 class SignUp (UserCreationForm):
 
     first_name = forms.CharField (max_length=30, widget = forms.TextInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'Alazar',
         'autocomplate' : 'off'
     }))
     last_name = forms.CharField (max_length=30, widget= forms.TextInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'Bekele',
         'autocomplate' : 'off'
     }))
     Email = forms.EmailField (max_length=30, widget= forms.EmailInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'example@gmail.com',
         'autocomplate' : 'off'
     }))
     username = forms.CharField (max_length=30, widget= forms.TextInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'alazarbekele11',
         'autocomplate' : 'off'
     }))
     password1 = forms.CharField (max_length=30, label='password Confirm' ,widget= forms.PasswordInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'password',
         'autocomplate' : 'off'
     }))
     password2 = forms.CharField (max_length=30, label='password Confirm' ,widget= forms.PasswordInput (attrs={
-        'class' : 'form-control',
+        'class' : 'form-control ps-5 pe-5',
         'placeholder' : 'confirm password',
         'autocomplate' : 'off'
     }))
