7c9135b3118bce5c185ca860223feec9b128ad69
AlazarBekele <alazarthe70@gmail.com>
2024-12-23 18:57:20 +0300

    fix all login and sign up pages form

--- Chat/forms.py
+++ Chat/forms.py
@@ -6,32 +6,32 @@ from django.contrib.auth.models import User
 class SignUp (UserCreationForm):
 
     first_name = forms.CharField (max_length=30, widget = forms.TextInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
-        'placeholder' : 'Alazar',
+        'class' : 'form-control ps-2',
+        'placeholder' : 'Last Name',
         'autocomplate' : 'off'
     }))
     last_name = forms.CharField (max_length=30, widget= forms.TextInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
-        'placeholder' : 'Bekele',
+        'class' : 'form-control ps-2 sizeWidth',
+        'placeholder' : 'Last Name',
         'autocomplate' : 'off'
     }))
     Email = forms.EmailField (max_length=30, widget= forms.EmailInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
+        'class' : 'form-control ps-2',
         'placeholder' : 'example@gmail.com',
         'autocomplate' : 'off'
     }))
     username = forms.CharField (max_length=30, widget= forms.TextInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
+        'class' : 'form-control ps-2 sizeWidth',
         'placeholder' : 'alazarbekele11',
         'autocomplate' : 'off'
     }))
     password1 = forms.CharField (max_length=30, label='password Confirm' ,widget= forms.PasswordInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
+        'class' : 'form-control ps-2 sizeWidth',
         'placeholder' : 'password',
         'autocomplate' : 'off'
     }))
     password2 = forms.CharField (max_length=30, label='password Confirm' ,widget= forms.PasswordInput (attrs={
-        'class' : 'form-control ps-5 pe-5',
+        'class' : 'form-control ps-2 sizeWidth',
         'placeholder' : 'confirm password',
         'autocomplate' : 'off'
     }))
@@ -39,3 +39,22 @@ class SignUp (UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'Email', 'username', 'password1', 'password2')
+
+
+class LoginCheck (forms.Form):
+
+    username = forms.CharField (widget= forms.TextInput (attrs={
+
+        'class' : 'form-control',
+        'placeholder' : 'Username or password',
+        'type' : 'text'
+
+    }))
+
+    password = forms.CharField (widget= forms.PasswordInput (attrs= {
+
+        'class' : 'form-control',
+        'placeholder' : 'password',
+        'type' : 'password'
+        
+    }))
--- Chat/views.py
+++ Chat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import SignUp
+from .forms import SignUp, LoginCheck
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
@@ -11,19 +11,13 @@ def home (request):
 
 def logPage (request):
 
-    return render (request, 'Login.html')
-
-
-
-def signup (request):
-
-    form = SignUp(request.POST or None)
+    log = LoginCheck(request.POST or None)
 
     if request.method == 'POST':
-        if form.is_valid():
+        if log.is_valid():
 
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = log.cleaned_data.get('username')
+            password = log.cleaned_data.get('password')
 
             user = authenticate (request, username=username, password=password)
 
@@ -32,7 +26,25 @@ def signup (request):
                 return redirect ('login')
     
     context = {
-        'form' : form
+        'log' : log
+    }
+
+    return render (request, 'Login.html', context)
+
+
+
+def signup (request):
+
+    signin = SignUp(request.POST or None)
+
+    if request.method == 'POST':
+        if signin.is_valid():
+
+            signin.save()
+            return redirect ('login')
+        
+    context = {
+        'signin' : signin
     }
 
     return render (request, 'Signup.html', context)
